a9610af495d162808c5c9229bba9e044893af8d6
joilson <joilsonhungrianv@gmail.com>
2023-08-01 14:41:37 -0300

    fex: removing unising useState in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
-import { useState } from "react";
+import {} from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <h1>TESTE</h1>
