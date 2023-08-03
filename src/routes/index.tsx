9615398202bba4eef67489ee000e897f784c9d27
joilson <joilsonhungrianv@gmail.com>
2023-08-03 11:46:29 -0300

    feat: implement rout dashboard

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import { HomePage } from "../pages/HomePage";
 import { LoginPage } from "../pages/LoginPage";
 import { RegisterPage } from "../pages/RegisterPage";
+import { DashBoardPage } from "../pages/DashBoardPage";
 
 export const RoutesMain = () => {
   return (
@@ -9,6 +10,7 @@ export const RoutesMain = () => {
       <Route path="/" element={<HomePage />} />
       <Route path="/Login" element={<LoginPage />} />
       <Route path="/Register" element={<RegisterPage />} />
+      <Route path="/DashBoard" element={<DashBoardPage />} />
     </Routes>
   );
 };
