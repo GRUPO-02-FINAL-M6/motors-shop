a34944baefb1480fd9e288e9ad0f4b4fd9e125ed
joilson <joilsonhungrianv@gmail.com>
2023-08-03 11:18:35 -0300

    feat: implement reset style

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { ResetStyle } from "./styles/resetStyles.ts";
+import { GlobalStyle } from "./styles/globalStyles.ts";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
+    <ResetStyle />
+    <GlobalStyle />
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
