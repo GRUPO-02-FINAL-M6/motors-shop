09a36dd093d662c41b178ef86498c3675adcf6f6
José Gabriel <gabriel.souzamorais@gmail.com>
2023-08-08 10:44:53 -0300

    feat: add cards & adsProvider

--- src/pages/dashboard/DashBoardPage.tsx
+++ src/pages/dashboard/DashBoardPage.tsx
@@ -1,3 +1,16 @@
+import { useContext } from "react";
+import { AdsContext } from "../../providers/adsProvider";
+import { Card } from "../../components/Card";
+
 export const DashBoardPage = () => {
-  return <h1>DashBoard Page</h1>;
+  const {ads} = useContext(AdsContext)
+
+  return (
+    <>
+    <h1>DashBoard Page</h1>
+    {
+      ads.map(ads => <Card ads={ads} key={ads.id}/>)
+    }
+    </>
+  );
 };
--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,18 +1,3 @@
-import { useContext } from "react";
-import { Card } from "../../components/Card";
-import { AdsContext } from "../../providers/adsProvider";
-
 export const HomePage = () => {
-  const { ads } = useContext(AdsContext);
-
-  return (
-    <>
-      <div>DASHBOARD</div>
-      <ul>
-        {ads.map((ads) => (
-          <Card ads={ads} key={ads.id} />
-        ))}
-      </ul>
-    </>
-  );
+  return <div>Home page</div>;
 };
