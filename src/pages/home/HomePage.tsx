2160b085d1543e7d5ea66a18654a6046ee230407
José Gabriel <gabriel.souzamorais@gmail.com>
2023-08-11 10:53:47 -0300

    feat: add consulta fipe

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { AdsContext } from "../../providers/adsProvider";
 import {
   ButtonLogin,
@@ -15,19 +15,42 @@ import { useNavigate } from "react-router-dom";
 
 export const HomePage = () => {
   const [menuStatus, setMenuStatus] = useState(false);
-  const { ads } = useContext(AdsContext);
-  const navigate = useNavigate()
+  const { ads, createAds } = useContext(AdsContext);
+  const navigate = useNavigate();
 
   const toggleMenu = () => {
     setMenuStatus(!menuStatus);
   };
+
+  const x = {
+    name: "bolt ev premier 203cv (elétricO)",
+    brand: "chevrolet",
+    color: "branco",
+    description:
+      "Um dos carros que fez a fama da Hyundai no Brasil está de volta em nova geração. O Hyundai Azera 2020 começa a ser vendido pela importadora oficial Caoa, que cumpre assim a promessa de trazê-lo no 1º semestre deste ano. Chega em versão única com motor 3.0 V6 de 261 cv, câmbio automático de 8 marchas e o nada modesto preço de R$ 269.900.",
+    km: 50000,
+    fuel: "Gasolina / Etanol",
+    price: 40000,
+    createdAt: "2023-08-08",
+    images: [
+      "https://www.autocenterguapore.com.br/carros/b8800e3389102ed2867c71c1510032b5-thumbjpeg-hyundai-i30-8974161-1000-750-70.jpg",
+      "https://www.autocenterguapore.com.br/carros/75cac425ef0d005436ed1ed451b749f4-thumbjpeg-hyundai-i30-8974161-1000-750-70.jpg",
+    ],
+    year: 2022,
+    modelCar: "Hyundai",
+  };
+
   return (
     <>
       <HeaderProps menuOpen={menuStatus}>
         <img src="../../src/assets/logo.svg" alt="" />
         <ButtonsProps menuOpen={menuStatus}>
-          <ButtonLogin onClick={() => navigate("login")}>Fazer Login</ButtonLogin>
-          <ButtonRegister onClick={() => navigate("register")}>Cadastrar</ButtonRegister>
+          <ButtonLogin onClick={() => navigate("login")}>
+            Fazer Login
+          </ButtonLogin>
+          <ButtonRegister onClick={() => navigate("register")}>
+            Cadastrar
+          </ButtonRegister>
         </ButtonsProps>
         <HamburgerIcon onClick={toggleMenu}>
           {menuStatus ? <h2>X</h2> : <h2>☰</h2>}
@@ -41,27 +64,28 @@ export const HomePage = () => {
           </div>
           <img src={imagem} alt="background image" />
         </div>
-        <AdsSectionStyled>
-            {
-              ads.length > 0 ? (
-                <ul>
-                  {
-                    ads.map((ads) => (
-                      <Card ads={ads} key={ads.id} />
-                    ))
-                  }
-                </ul>
-              ) : (
-                <div id="empty">
-                  <h2>Ainda não há anúncios cadastrados...</h2>
-                </div>
-              )
-            }
-        </AdsSectionStyled>
+        <section id="main-section">
+          <div><h1>FILTRO AQUI...</h1></div>
+          <AdsSectionStyled>
+            {ads.length > 0 ? (
+              <ul>
+                {ads.map((ads) => (
+                  <Card ads={ads} key={ads.id} />
+                ))}
+              </ul>
+            ) : (
+              <div id="empty">
+                <h2>Ainda não há anúncios cadastrados...</h2>
+              </div>
+            )}
+          </AdsSectionStyled>
+        </section>
         <div id="main-bottom">
-          <ButtonFilters/>
-              <h3>1 de 2</h3>
-            <button id="next-page">Seguinte</button>
+          <ButtonFilters />
+          <h3>1 de 2</h3>
+          <button id="next-page" onClick={() => createAds(x)}>
+            Seguinte
+          </button>
         </div>
       </MainStyled>
     </>
--- src/providers/adsProvider.tsx
+++ src/providers/adsProvider.tsx
@@ -1,53 +1,94 @@
 import React, { createContext, useEffect, useState } from "react";
-import { api } from "../services/api";
+import { api, apiFipe } from "../services/api";
 import { toast } from "react-toastify";
-import axios from "axios";
+import axios, { AxiosResponse } from "axios";
 
 interface iAdsProviderProps {
-    children: React.ReactNode
+  children: React.ReactNode;
 }
 
 interface iAdsContext {
-    ads: iAds[]
+  ads: iAds[];
+  createAds: (payload: iAdsRequest) => Promise<AxiosResponse<any, any> | undefined>
 }
 
 export interface iAds {
-    id: number
-    name: string
-    brand: string
-    description: string
-    year: number
-    km: number
-    images: string[]
-    fuel: string
-    createdAt: string
-    value: number
-    user: {name: string}
+  id: number;
+  name: string;
+  brand: string;
+  description: string;
+  year: number;
+  km: number;
+  color: string;
+  images: string[];
+  fuel: string;
+  createdAt: string;
+  user: { name: string };
+  price: number;
+  priceFip: number;
+  modelCar: string;
 }
 
-export const AdsContext = createContext({} as iAdsContext)
+export interface iAdsRequest {
+  name: string;
+  brand: string;
+  description: string;
+  year: number;
+  km: number;
+  color: string;
+  images: string[];
+  fuel: string;
+  createdAt: string;
+  user: { name: string };
+  price: number;
+  priceFip: number;
+  modelCar: string;
+}
+
+export const AdsContext = createContext({} as iAdsContext);
 
 export const AdsProvider = ({ children }: iAdsProviderProps) => {
-    const [ads, setAds] = useState<iAds[]>([]);
+  const [ads, setAds] = useState<iAds[]>([]);
+  const token = localStorage.getItem("token")
 
-    useEffect(() => {
-        getAds();
-    }, []);
+  useEffect(() => {
+    getAds();
+  }, []);
 
-    const getAds = async () => {
-        try {
-            const response = await api.get("/advertisement");
-            setAds(response.data);
-        } catch (error) {
-            if(axios.isAxiosError(error)){
-                toast.error(error.response?.data)
-            };
-        }
-    };
-    
-    return (
-        <AdsContext.Provider value={{ ads }}>
-            {children}
-        </AdsContext.Provider>
-    );
+  const getFipeValue = async (brand: string, name: string, year: number, fuel: number) => {
+    try {
+      const response = await apiFipe.get(
+        `/cars/unique?brand=${brand}&name=${name}&year=${year}&fuel=${fuel}`
+      );
+      console.log(response.data.value)
+      return response.data.value
+    } catch (error) {
+        toast.error('Veículo não encontrado na tabela FIPE')
+    }
+  };
+
+  const createAds = async(payload: iAdsRequest) => {
+    try {
+        payload.priceFip = await getFipeValue(payload.brand, payload.name, payload.year, 3)
+        const response = await api.post("/advertisement", payload, {
+            headers: {Authorization: `Bearer ${token}`}
+        })
+        return response
+    } catch (error: any) {
+        toast.error(error.response.data.message)
+    }
+  }
+
+  const getAds = async () => {
+    try {
+      const response = await api.get("/advertisement");
+      setAds(response.data);
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        toast.error(error.response?.data);
+      }
+    }
+  };
+
+  return <AdsContext.Provider value={{ ads, createAds }}>{children}</AdsContext.Provider>;
 };
